f7b2f6540b87cd59e7805f3f000dcc6af4dc8d0d
rzhang15 <rzhang15@exseed.ed.ac.uk>
2020-10-06 15:11:15 +0100

    update other RDFs

--- otherRDFs.py
+++ otherRDFs.py
@@ -2,12 +2,12 @@
 Other types of RDF as benchmark to the extend RDF
 '''
 
-
+import numpy as np
 from matminer.featurizers.structure import RadialDistributionFunction
 from matminer.featurizers.utils.grdf import Histogram as PartialRDF
 
 
-def origin_rdf_histo(structure, max_dist=10, bin_size=0.1):
+def origin_rdf_histo(structures, max_dist=10, bin_size=0.1):
     '''
     Calcualte the vanilla RDF using matminer.  
     BEWARE! In the implementation of RDF in matminer, the endpoint
@@ -16,13 +16,19 @@ def origin_rdf_histo(structure, max_dist=10, bin_size=0.1):
     shorter than the extend RDF.
     
     Args:
-        struct: pymatgen structure
+        structures: a list of pymatgen structures
     Return:
-        a nd array containing histogram of RDF
+        a ndarray with dimension [x,y], x is number of compounds i.e. 
+        the length of the input list, y is the number of histogram 
+        points of RDF
     '''
-    rdf_fn = RadialDistributionFunction(cutoff=max_dist, bin_size=bin_size)  
-    RDFs = rdf_fn.featurize(structure)  
-    return RDFs[1]
+    rdf_fn = RadialDistributionFunction(cutoff=max_dist, bin_size=bin_size)
+    results = rdf_fn.featurize_many(structures)
+
+    rdfs = []
+    for result in results:
+        rdfs.append(result[0]['distribution'])
+    return np.stack(rdfs)
 
 
 def partial_rdf():
