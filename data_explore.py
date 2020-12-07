acae3c1eb9e05e606ddba6230f6355c6a44a43e7
rzhang15 <rzhang15@exseed.ed.ac.uk>
2020-12-07 18:07:12 +0000

    add rdf similarity, bond statistics per site

--- data_explore.py
+++ data_explore.py
@@ -5,20 +5,19 @@ import json
 import time
 import gzip
 import argparse
-from collections import Counter
-from itertools import combinations
-from pandas import DataFrame
+from scipy.stats import wasserstein_distance
 from pymatgen import Structure
-from pymatgen.core.periodic_table import Element
 from pymatgen.analysis.local_env import CrystalNN
-from sklearn.preprocessing import MultiLabelBinarizer
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer 
 try:
     from matminer.featurizers.structure import StructuralComplexity
 except:
     print('matminer is not installed, cannot calculate structural complexity')
 
 from extendRDF import rdf_histo, rdf_kde, get_rdf_and_atoms, shell_similarity
+from composition import elements_count, elements_selection, bonding_type
 from otherRDFs import origin_rdf_histo
+from rdf_io import rdf_read
 
 
 def batch_rdf(data, max_dist=10, bin_size=0.1, method='bin', normalize=True, 
@@ -129,6 +128,7 @@ def rdf_trim(all_rdf, trim='minimum'):
 def rdf_flatten(all_rdf):
     '''
     If the rdf is not 1d, make it 1d for machine learning input
+    This is done after trim so np.stack can be used
 
     Args:
         all_rdf: a 2D or 3D np array (rdfs) with the same length, 
@@ -138,7 +138,7 @@ def rdf_flatten(all_rdf):
     '''
     if len(all_rdf[0].shape) == 2:
         all_rdf = [ x.flatten() for x in all_rdf]
-    return all_rdf
+    return np.stack(all_rdf)
 
 
 def bond_length_statis(structure): 
@@ -157,27 +157,45 @@ def bond_length_statis(structure):
     '''
     nn = CrystalNN()
     bond_len = []
-    for j in range(len(structure)) : 
-        for i in nn.get_nn_info(structure, j) :       
+    for j in range(len(structure)): 
+        for i in nn.get_nn_info(structure, j):       
             bond_len.append(structure[j].distance(structure[i['site_index']]))
     bond_len = np.array(bond_len)
     return bond_len.mean(), bond_len.std()#, bond_len.ptp()
 
 
-def average_coordination(structrue):
+def average_coordination(structure):
     '''
     Calculation of average coordination number over every site in a structure
     using Vorini method
 
     Args:
-        structrue: pymatgen structure
+        structure: pymatgen structure
     Return:
         Average coordination number
     '''
+    nn = CrystalNN()
     ave_coord_num = []
-    for atom_site in range(len(structure)) :
+    for atom_site in range(len(structure)):
         ave_coord_num.append(len(nn.get_nn_info(structure, atom_site)))
     return np.array(ave_coord_num).mean()
+
+
+def bond_stat_per_site(structure):
+    '''
+    '''
+    nn = CrystalNN()
+    coord_num = []
+    bond_len_std = []
+    for atom_site in range(len(structure)):
+        struct_nn = nn.get_nn_info(structure, atom_site)
+        coord_num.append(len(struct_nn))
+        bond_len = []
+        for i in struct_nn:       
+            bond_len.append(structure[atom_site].distance(structure[i['site_index']]))
+        bond_len_std.append(np.array(bond_len).std())
+    return round(np.array(bond_len_std).mean(), 3), \
+            round(np.array(coord_num).std(), 3)
 
 
 def num_of_shells(data, dir):
@@ -240,284 +258,62 @@ def rdf_value_stat(data, dir):
     return
 
 
-def composition_one_hot(data, only_type=False, normalize=True):
+def rdf_similarity(data, all_rdf, order='symmetry'):
     '''
-    Make the composition to fix size vector like one hot array
+    Calculate the earth mover's distance between two RDFs
+    Current support vanilla rdf and extend rdf
+    Using Guassian smearing for rdf
 
     Args:
-        data: the bulk modulus data and structure from Materials Project
-        only_type: if true, only the types are included but not  
-            the whole formula
-        normalize: if normalize, the number of atoms in the output will 
-            be given as percentage
+        data: data from json
+        all_rdf: 
+        order: how the compounds are arranged
+            symmetry: in the order of space group number, typically for 
+                influence of distortion
+            lattice: in the order of lattice constant
     Return:
-        1. one hot reprentation in atomic array
-        2. the order of the elements in the list
+        a pandas dataframe with all pairwise distance
+        for multiple shells rdf the distance is the mean value of all shells
     '''
-    pero_tab_nums = []
-    if only_type:
-        mlb = MultiLabelBinarizer()
+    if len(all_rdf[0].shape) == 2:
+        # typically for extend RDF
+        df = pd.DataFrame([])
+        for i1, d1 in enumerate(data):
+            for i2, d2 in enumerate(data):
+                if i1 < i2:
+                    rdf_len = np.array([len(all_rdf[i1]), len(all_rdf[i2])]).min()
+                    shell_distances = []
+                    for j in range(rdf_len):
+                        shell_distances.append(wasserstein_distance(all_rdf[i1][j], all_rdf[i2][j]))
+                    df.loc[d1['task_id'], d2['task_id']] = np.array(shell_distances).mean()
+                    df.loc[d2['task_id'], d1['task_id']] = np.array(shell_distances).mean()
+    elif len(all_rdf[0].shape) == 1:
+        # typically for vanilla RDF
+        df = pd.DataFrame([])
+        for i1, d1 in enumerate(data):
+            for i2, d2 in enumerate(data):
+                if i1 < i2:
+                    shell_distance = wasserstein_distance(all_rdf[i1], all_rdf[i2])
+                    df.loc[d1['task_id'], d2['task_id']] = shell_distance
+                    df.loc[d2['task_id'], d1['task_id']] = shell_distance    
+    
+    if order == 'symmetry':
+        df2 = pd.DataFrame(columns=['sg'])
         for d in data:
             struct = Structure.from_str(d['cif'], fmt='cif')
-            species = struct.types_of_specie
-            pero_tab_nums.append([ x.number for x in species ])
-        #
-        data_np = mlb.fit_transform(pero_tab_nums)
-        elem_numbers = mlb.classes_.tolist()
-        elem_symbols = [ Element.from_Z(x).name for x in elem_numbers ]
-        return data_np, elem_symbols
-    else:
+            df2.loc[d['task_id']] = struct.get_space_group_info()[1]
+        new_index = df2.sort_values('sg').index.values
+    elif order == 'lattice':
+        df2 = pd.DataFrame(columns=['lattice'])
         for d in data:
             struct = Structure.from_str(d['cif'], fmt='cif')
-            species = struct.species
-            pero_tab_nums.append([ x.number for x in species ])
-        # use the counter method to one-hot the element numbers
-        data_pd = pd.DataFrame([Counter(x) for x in pero_tab_nums])
-        data_np = data_pd.fillna(0).sort_index(axis=1).to_numpy()
-        if normalize:
-            data_np = data_np/data_np.sum(axis=1, keepdims=True)
+            df2.loc[d['task_id']] = struct.lattice.a
+        new_index = df2.sort_values('lattice').index.values
 
-        # get the order of elements in the list and element symbols
-        elem_numbers = data_pd.sort_index(axis=1).columns.tolist()
-        elem_symbols = [ Element.from_Z(x).name for x in elem_numbers ]
-        return np.ndarray.round(data_np, 3), elem_symbols
-
-
-def elements_count(data):
-    '''
-    Count the elements distribution histogram in the compounds dataset,  
-    and the covariance matrix of each pair of elements
-
-    Args:
-        data: a list of dicts with CIFs
-    Return:
-        write element histogram in elem_histo file
-        write element-wise covariance matrix in elem_matrix file
-    '''
-    # This is the periodic table which DFT pesudopotentials are avaiable
-    periodic_table = [
-        'H',  'He', 
-        'Li', 'Be', 'B',  'C',  'N',  'O',  'F',  'Ne', 
-        'Na', 'Mg', 'Al', 'Si', 'P',  'S',  'Cl', 'Ar', 
-        'K',  'Ca', 'Sc', 'Ti', 'V',  'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 
-        'Rb', 'Sr', 'Y',  'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I',  'Xe', 
-        'Cs', 'Ba', 
-                    'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 
-                          'Hf', 'Ta', 'W',  'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 
-                    'Ac', 'Th', 'Pa', 'U',  'Np', 'Pu']
-    # initialize two dicts
-    elem_histo = {}
-    elem_matrix = {}
-    for elem in periodic_table:
-        elem_histo[elem] = 0
-        elem_matrix[elem] = {}
-        for elem2 in periodic_table:
-            elem_matrix[elem][elem2] = 0
-
-    for d in data:
-        elements = Structure.from_str(d['cif'], fmt='cif').symbol_set
-        for elem in elements:
-            elem_histo[elem] += 1
-        for elem_pair in combinations(elements, 2):
-            elem_matrix[elem_pair[0]][elem_pair[1]] += 1
-
-    for elem in periodic_table:
-        for elem2 in periodic_table:
-            if elem_matrix[elem][elem2] == 0 and elem != elem2:
-                elem_matrix[elem][elem2] = elem_matrix[elem2][elem]
-
-    elem_histo = {elem:count for elem,count in elem_histo.items() if count != 0}
-    elem_histo = DataFrame([elem_histo])
-    elem_histo = elem_histo[(elem_histo != 0)]
-    elem_histo.T.to_csv('elem_histo', sep=' ')
-
-    #
-    elem_matrix = DataFrame.from_dict(elem_matrix)
-    # remove columns and rows with all zero
-    elem_matrix = elem_matrix.loc[:, (elem_matrix != 0).any(axis=0)] # column
-    elem_matrix = elem_matrix[(elem_matrix != 0).any(axis=1)]   # row
-    elem_matrix.to_csv('elem_matrix', sep=' ')
-
-    return
-
-
-def elements_selection(data, elem_list, mode='include', method='any'):
-    '''
-    Select a subset contains or not contain certain elements.
-
-    Args:
-        data: a list of dicts with CIFs
-        elem_list: a list of the elements of interest or no-interest
-        mode:
-            include: select the structures have elements in the elem_list
-            exclude: drop the structures have elements in the elem_list
-        method:
-            all: the structure has all the elements in the list
-            any: the structure has any one element in the list
-    Return:
-    '''
-    for d in data[:]:
-        elements = Structure.from_str(d['cif'], fmt='cif').symbol_set
-        if mode == 'include':
-            if set(elem_list).isdisjoint(elements):
-                data.remove(d)
-        elif mode == 'exclude':
-            if not set(elem_list).isdisjoint(elements):
-                data.remove(d)
-
-    return data
-
-
-def similarity_matrix(input_file='dist_matrix', normalize='inverse', order='pt_number'):
-    '''
-    Read the original file from the below reference, and convert into a dictionary
-
-    Hautier, G., et al. (2011). 
-    "Data mined ionic substitutions for the discovery of new compounds." 
-    Inorganic Chemistry 50(2): 656-663.
-
-    Args:
-        input_file: the preprocessed supporting txt file (from the above paper)
-                using the shell script below
-        normalize: method for value normalization
-            bound: all value divide by 20 (based on the maximum value)
-            log: all value got log10
-            inverse: 1 / values
-        order: the order of element in the matrix
-            pt_number: the order in periodic table, i.e. atomic number
-                    typically for calculation purpose
-            pettifor: typically for visualization purpose, following 
-                    Pettifor, D. G. (1990). "Structure maps in alloy design." 
-                    Journal of the Chemical Society, Faraday Transactions 86(8): 1209.
-     Return:
-        a pandas dataframe of the similarity matrix
-    ====================================================
-    #!/bin/sh
-    outfile=dist_matrix
-    > $outfile
-    for i in `seq 78` # up to Bi and remove nobel gas
-    do
-        e1=`sed -n "$i p" mendlev` # element symbols in Pettifor order
-        p1=`sed -n "$i p" Pettifor` # element symbol + valency in Pettifor order
-        for j in `seq 78`
-        do
-            e2=`sed -n "$j p" mendlev`
-            p2=`sed -n "$j p" Pettifor`
-            if [ $i -gt $j ]
-            then
-                r=`grep $p1 ic102031h_si_001.txt | grep $p2`
-                if [ -z "$r" ]
-                then
-                    grep -w $e1 ic102031h_si_001.txt | grep -w $e2 | head -n 1 >> $outfile
-                else
-                    echo $r >> $outfile
-                fi
-            fi
-        done
-    done
-    sed -i 's/:/ /g' $outfile # make the valency a seperate column
-    =========================================================
-    '''
-    # note that the index_col is after the use selection
-    d = pd.read_csv(input_file, sep=' ', index_col=[0,1], usecols=[0,2,4])
-    d = d.unstack()
-    # drop the multilevel when ustack
-    d.columns = d.columns.droplevel()
-
-    # Pettifor order of elements
-    if order == 'pt_number':
-        index = ['H', 'Li', 'Be', 'B',  'C',  'N',  'O',  'F', 'Na', 'Mg', 'Al', 'Si', 'P',  'S',  'Cl', 
-                'K',  'Ca', 'Sc', 'Ti', 'V',  'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 
-                'Rb', 'Sr', 'Y',  'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 
-                'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 
-                'Hf', 'Ta', 'W',  'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi']
-    elif order == 'pettifor':
-        index = ['Cs', 'Rb', 'K', 'Na', 'Li', 'Ba', 'Sr', 'Ca', 'Yb', 'Eu', 'Y',  'Sc', 'Lu', 'Tm', 'Er', 'Ho', 
-                'Dy', 'Tb', 'Gd', 'Sm', 'Pm', 'Nd', 'Pr', 'Ce', 'La', 'Zr', 'Hf', 'Ti', 'Nb', 'Ta', 'V',  'Mo', 
-                'W',  'Cr', 'Tc', 'Re', 'Mn', 'Fe', 'Os', 'Ru', 'Co', 'Ir', 'Rh', 'Ni', 'Pt', 'Pd', 'Au', 'Ag', 
-                'Cu', 'Mg', 'Hg', 'Cd', 'Zn', 'Be', 'Tl', 'In', 'Al', 'Ga', 'Pb', 'Sn', 'Ge', 'Si', 'B',  'Bi', 
-                'Sb', 'As', 'P',  'Te', 'Se', 'S', 'C', 'I', 'Br', 'Cl', 'N', 'O', 'F', 'H']
-    # reindex, i.e. change the order of column and rows to Pettifor order  
-    d = d.fillna(0)
-    d = d.reindex(columns=index, fill_value=0) 
-    d = d.reindex(index=index, fill_value=0)
-    d = d + d.transpose()
-    # the maximum similarity number is 18.6, set the same element to 20, a bit arbitary
-    np.fill_diagonal(d.values, 20)
-    # then fill the zeros
-    d.loc[:,'Pm'] = d.loc[:,'Sm'] # column, same as:  d['Pm'] = d['Sm']
-    d.loc['Pm',:] = d.loc['Sm',:] # row
-    # other zero mean very dissimilar, so set to a very small value
-    d.replace(0, 0.1, inplace=True)
-
-    if normalize == 'bound':
-        d = d / 20
-    elif normalize == 'log':
-        d = np.log10(d)
-    elif normalize == 'inverse':
-        d = 1 / d
-        np.fill_diagonal(d.values, 0)
-    else:
-        print('normalization method not supported')
-
-    return d
-
-
-def bonding_type(structure):
-    '''
-    Calculate bonding in a given structure.
-
-    Args:
-        structure: a pymatgen structure
-    Return:
-        A list of atomic pairs which form bonding
-    '''
-    nn = CrystalNN()
-    bond_elem_list = []
-    bond_num_list = []
-    for i in list(range(len(struct))):
-        site1 = struct[i].species_string
-        num1 = struct[i].specie.number
-        for neigh in nn.get_nn_info(struct, i):
-            bond_elem_list.append(' '.join(sorted([site1, neigh['site'].species_string])))
-            bond_num_list.append(' '.join(list(map(str, sorted([num1, neigh['site'].specie.number])))))
-
-    bond_elem_list = list(set(bond_elem_list))
-    bond_num_list = list(set(bond_num_list))
-   
-    return bond_elem_list, bond_num_list
-            
-
-def bonding_matrix(data):
-    '''
-    Convert the atomic pair of bonding into a matrix representation
-    Remove the elements that don't exsit, then flatten it.
-
-    Args:
-        data: a list of dicts with CIFs
-    Return:
-        A 2D vector, first dimension is number of samples.
-    '''
-    periodic_table = ['H', 'Li', 'Be', 'B',  'C',  'N',  'O',  'F', 'Na', 'Mg', 'Al', 'Si', 'P',  'S',  'Cl', 
-            'K',  'Ca', 'Sc', 'Ti', 'V',  'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 
-            'Rb', 'Sr', 'Y',  'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 
-            'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 
-            'Hf', 'Ta', 'W',  'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi']
-
-    all_bond_matrix = []
-    num_elements = len(periodic_table)
-    zeor_bond_matrix = pd.DataFrame(np.zeros((num_elements, num_elements)), 
-                                    index=periodic_table, columns=periodic_table)
-    for d in data:
-        bond_matrix = zeor_bond_matrix
-        bond_list = d['bond_elem_list']
-        for bond in bond_list:
-            elem1, elem2 = bond.split()
-            bond_matrix.loc[elem1, elem2] = 1
-            bond_matrix.loc[elem2, elem1] = 1
-        all_bond_matrix.append(bond_matrix.values.flatten())
-    # HERE NEED TO DELETE ZERO ROW/COLUMN AND THEN FLATTEN
-    return np.stack(all_bond_matrix)
+    df = df.reindex(columns=new_index, index=new_index, fill_value=0)
+    np.fill_diagonal(df.values, 0)
+    
+    return df 
 
 
 if __name__ == '__main__':
@@ -535,6 +331,8 @@ if __name__ == '__main__':
                             '   origin_rdf: calcualte vanilla RDF of all the CIFs \n' + 
                             '   composition: element-wise statistics of all compositions \n' +
                             '   bonding_type: \n' +
+                            '   shell_similarity: \n' +
+                            '   rdf_similarity: \n' +
                             '   subset: select a subset which have specified elements'
                       )
     parser.add_argument('--elem_list', type=str, default='O',
@@ -565,11 +363,27 @@ if __name__ == '__main__':
         origin_rdf_histo(data, max_dist=max_dist)
     elif task == 'composition':
         elements_count(data)
+    elif task == 'shell_similarity':
+        for i, d in enumerate(data):
+            if ( i % 100 ) == 0:
+                print(i) 
+            with open(rdf_dir + '/' + d['task_id']) as f:
+                rdf = np.loadtxt(f, delimiter=' ')
+                np.savetxt('../shell_similarity/' + d['task_id'], 
+                            shell_similarity(rdf[:30]), 
+                            delimiter=' ', fmt='%.3f')
     elif task == 'bonding_type':
         for d in data:
             struct = Structure.from_str(d['cif'], fmt='cif')
             d['bond_elem_list'], d['bond_num_list'] = bonding_type(struct)
         with open(infile.replace('.json','_with_bond.json'), 'w') as f:
+            json.dump(data, f, indent=1)
+    elif task == 'bond_stat_per_site':
+        for d in data:
+            struct = Structure.from_str(d['cif'], fmt='cif')
+            d['ave_bond_std'], d['coord_num_std'] = bond_stat_per_site(struct)
+            d['num_sg_operation'] = len(SpacegroupAnalyzer(struct).get_space_group_operations())
+        with open(infile.replace('v7','v8'), 'w') as f:
             json.dump(data, f, indent=1)
     elif task == 'subset':
         print(elem_list)
@@ -578,6 +392,14 @@ if __name__ == '__main__':
         # note that 'data' is also changed because it is defined in __main__
         with open('subset.json', 'w') as f:
             json.dump(subset, f, indent=1)
+    elif task == 'rdf_similarity':
+        with open (infile,'r') as f:
+            data = json.load(f)
+
+        all_rdf = rdf_read(data, rdf_dir)
+        df = rdf_similarity(data, all_rdf)
+        df.to_csv('../rdf_similarity.csv')
+   
     else:
         print('This task is not supported')
 
