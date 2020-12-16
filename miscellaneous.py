5f930e483c92ccf98b2cd5825ea5edb11d2f20e1
rzhang15 <rzhang15@ed.ac.uk>
2020-12-16 09:55:49 +0000

    add perovskite distortion

--- miscellaneous.py
+++ miscellaneous.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 import pandas as pd
 from collections import Counter
+from pymatgen import Structure
 
 try:
     from ElMD import ElMD
@@ -92,6 +93,54 @@ def analysis_emd_100():
             for val in range(4):
                 df.loc[thresh, i + str(val)] = np.count_nonzero(data == val)
     df.to_csv('analysis.csv')
+
+
+def json_order():
+    '''
+    Make the perovskite structure in the order of lattice constant
+    '''
+
+    with open('pero2.json','r') as f:
+        data = json.load(f)
+
+    l = []
+    for d in data:
+        struct = Structure.from_str(d['cif'], fmt='cif')
+        l.append(struct.lattice.a)
+
+    df2 = pd.DataFrame(l, columns=['lattice'])
+    new_index = df2.sort_values('lattice').index.values
+
+    d2 = []
+    for i in new_index:
+        d2.append(data[i])
+
+    with open('pero3.json','w') as f:
+        json.dump(d2,f,indent=1)
+
+
+def make_distorted_perovskite():
+    cif = "# generated using pymatgen\ndata_SrTiO3\n_symmetry_space_group_name_H-M   'P 1'\n_cell_length_a   3.94513000\n_cell_length_b   3.94513000\n_cell_length_c   3.94513000\n_cell_angle_alpha   90.00000000\n_cell_angle_beta   90.00000000\n_cell_angle_gamma   90.00000000\n_symmetry_Int_Tables_number   1\n_chemical_formula_structural   SrTiO3\n_chemical_formula_sum   'Sr1 Ti1 O3'\n_cell_volume   61.40220340\n_cell_formula_units_Z   1\nloop_\n _symmetry_equiv_pos_site_id\n _symmetry_equiv_pos_as_xyz\n  1  'x, y, z'\nloop_\n _atom_site_type_symbol\n _atom_site_label\n _atom_site_symmetry_multiplicity\n _atom_site_fract_x\n _atom_site_fract_y\n _atom_site_fract_z\n _atom_site_occupancy\n  Sr  Sr0  1  0.00000000  0.00000000  0.00000000  1\n  Ti  Ti1  1  0.50000000  0.50000000  0.50000000  1\n  O  O2  1  0.50000000  0.00000000  0.50000000  1\n  O  O3  1  0.50000000  0.50000000  0.00000000  1\n  O  O4  1  0.00000000  0.50000000  0.50000000  1\n"
+    s = Structure.from_str(cif, fmt='cif')
+
+    posits = [[0.50, 0.50, 0.50],
+            [0.50, 0.50, 0.51],
+            [0.50, 0.50, 0.52],
+            [0.50, 0.50, 0.53],
+            [0.50, 0.51, 0.51],
+            [0.51, 0.51, 0.51]]
+
+    all_dict = []
+    for i, posit in enumerate(posits):
+        one_dict = {}
+        one_dict['task_id'] = 'generate-' + str(i)
+        s[1] = 'Ti', posit
+        one_dict['cif'] = s.to(fmt='cif')
+        all_dict.append(one_dict)
+
+    with open('pero_gene.json','w') as f:
+        json.dump(all_dict, f, indent=1)
+
 
 
 if __name__ == '__main__':
