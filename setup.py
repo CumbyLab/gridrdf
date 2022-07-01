e6bcf5353711f38636181b3615d8432a2176388c
jcumby <james.cumby@ed.ac.uk>
2022-07-01 10:17:45 +0100

    Updated setup.py to take requirements directly from requirements.txt

--- setup.py
+++ setup.py
@@ -13,6 +13,26 @@ import unittest
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
+# Requirements
+reqs_file = os.path.join(module_dir, "requirements.txt")
+with open(reqs_file) as f:
+    reqs_raw = f.read()
+reqs_list = [r.replace("==", ">=") for r in reqs_raw.split("\n")]
+
+# Optional requirements
+# Approach borrowed from matminer, https://github.com/hackingmaterials/matminer/blob/main/setup.py
+extras_file = os.path.join(module_dir, "requirements-optional.txt")
+with open(extras_file) as f:
+    extras_raw = f.read()
+extras_raw = [r for r in extras_raw.split("##") if r.strip() and "#" not in r]
+extras_dict = {}
+for req in extras_raw:
+    items = [i.replace("==", ">=") for i in req.split("\n") if i.strip()]
+    dependency_name = items[0].strip()
+    dependency_reqs = [i.strip() for i in items[1:] if i.strip()]
+    extras_dict[dependency_name] = dependency_reqs
+extras_list = [r for d in extras_dict.values() for r in d]
+
 if __name__ == "__main__":
     setup(
         name="gridrdf",
@@ -31,16 +51,8 @@ if __name__ == "__main__":
         },
         zip_safe=False,
         test_suite="tests",
-        install_requires=[
-            "scipy",
-            "numpy",
-            "pymatgen",
-            "pandas",
-            "scikit-learn",
-            "pyemd",
-            "matminer",
-            
-        ],
+        install_requires=reqs_list,
+        extras_require=extras_dict,
         classifiers=[
             "Programming Language :: Python",
             "Development Status :: 3 - Alpha",
